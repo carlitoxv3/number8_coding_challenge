6e5ea948480b97b8bd14d463ee08c0735c074ec6
Agustin <agustin@tap-commerce.com>
2021-09-11 19:27:56 -0300

    Add ServiceCollectionExtensions to use method AddMemoryDbContext

---
+++ CodingChallenge.Repositories/DBContexts/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CodingChallenge.Repositories.DBContexts.Extensions
+{
+    /// <summary>
+    /// Extensions of IServiceCollection to implement a DBContext in Memory
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInMemoryDbContext<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+            => services.AddDbContext<TDbContext>(opt
+                => opt.UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .ConfigureWarnings(w =>
+                    {
+                        w.Ignore(InMemoryEventId.TransactionIgnoredWarning);
+                    }
+                    ), ServiceLifetime.Transient);
+    }
+}
