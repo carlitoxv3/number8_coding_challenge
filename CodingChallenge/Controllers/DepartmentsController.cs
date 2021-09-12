f32333e8cb6738c2b2696273fcf748f18800f773
Agustin <agustin@tap-commerce.com>
2021-09-12 12:32:04 -0300

    Add DepartmentsController

---
+++ CodingChallenge/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentsController : Controller
+    {
+        private Services.Interfaces.IDepartmentService _departmentService;
+        public DepartmentsController(Services.Interfaces.IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Models.Department>> Get()
+        {
+            return await this._departmentService.GetAll();
+        }
+    }
+}
