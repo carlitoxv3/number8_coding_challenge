90d7734b8076bc0ba97caa0186b477b5d9889dca
Agustin <agustin@tap-commerce.com>
2021-09-12 12:31:22 -0300

    Create DepartmentService

---
+++ CodingChallenge.Services/DepartmentService.cs
@@ -0,0 +1,35 @@
+﻿using CodingChallenge.Models;
+using CodingChallenge.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Services
+{
+    public partial class DepartmentService : IDepartmentService
+    {
+        private Repositories.Interfaces.IDepartmentRepository _departmentRepository;
+        private Repositories.DBContexts.LocalDBContext _localDBContext;
+        public DepartmentService(
+            Repositories.DBContexts.LocalDBContext localDBContext
+            )
+        {
+            _localDBContext = localDBContext;
+        }
+
+        public async Task<IEnumerable<Department>> GetAll()
+        {
+            try
+            {
+                _departmentRepository = new Repositories.DepartmentRepository(this._localDBContext);
+                return await _departmentRepository.GetAll();
+            }
+            catch (Exception ex)
+            {
+                // TODO: Make log
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CodingChallenge.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Services.Extensions
+{
+    /// <summary>
+    /// Extensions of IServiceCollection to implement a dependency injection
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServicesInjection(this IServiceCollection services)
+        {
+            services.AddScoped<Interfaces.IDepartmentService, DepartmentService>();
+            return services;
+        }
+    }
+}
