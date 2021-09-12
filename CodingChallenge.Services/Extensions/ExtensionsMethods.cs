b37d494b595d8233508a5348d3a8f12646f369e9
Agustin <agustin@tap-commerce.com>
2021-09-12 17:18:23 -0300

    Implements ExtensionMethods to obtains Rounded Number

---
+++ CodingChallenge.Services/Extensions/ExtensionsMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Services.Extensions
+{
+    public static class ExtensionsMethods
+    {
+        /// <summary>
+        /// Returns the number with the round using step
+        /// </summary>
+        /// <param name="value">Number to round</param>
+        /// <param name="roundTo">Step to round</param>
+        /// <returns></returns>
+        public static double RoundCentimalTo(this double value, double roundTo)
+        {
+            return Math.Round(Math.Ceiling(value / roundTo) * roundTo, 2);
+        }
+    }
+}
