4d1321995655e2e6631723c8fc3e0d0419c43094
Agustin <agustin@tap-commerce.com>
2021-09-12 17:18:39 -0300

    Add Controller of Orders

---
+++ CodingChallenge/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+        private Services.Interfaces.IOrderService _orderService;
+        public OrderController(Services.Interfaces.IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [HttpPost("[action]")]
+        public async Task<ActionResult> SaveAndCalculate([FromBody][Required] Models.DTOs.Request.OrderRequestDTO orderRequestDTO)
+        {
+
+            try
+            {
+                if(!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                // Validate that contains a least one product
+                if (orderRequestDTO.Products == null || (orderRequestDTO.Products != null && !orderRequestDTO.Products.Any()))
+                    return BadRequest("The order must contains almost one product");
+
+                return Ok(await this._orderService.SaveAndCalculate(orderRequestDTO));
+            }
+            catch (Exception ex)
+            {
+                // TODO: log error stack tracke
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
